6c68eb6cd0fa006e6b20bd1d070facbf96c18c17
Sebastian Zartner <sebastianzartner@gmail.com>
2016-06-07 01:55:17 +0200

    Allowed to parse <div class="index widgeted"> indexes

--- scraper.php
+++ scraper.php
@@ -49,12 +49,21 @@
       'tagOmission' => 'Tag omission',
       'permittedParents' => 'Permitted parent elements',
       'domInterface' => 'DOM interface'
-    ]/*,
+    ],
     'ar' => [
+      'contentCategories' => 'Content categories',
+      'permittedContent' => 'Permitted content',
+      'tagOmission' => 'Tag omission',
+      'permittedParents' => 'Permitted parent elements',
+      'domInterface' => 'DOM interface'
     ],
     'bn-BD' => [
-      'permittedContent' => 'Permitted content'
-    ]*/,
+      'contentCategories' => 'কনটেন্ট ক্যাটাগরি সমূহ|কন্টেন্ট ক্যাটাগরি',
+      'permittedContent' => 'অনুমোদিত কন্টেন্ট',
+      'tagOmission' => 'Tag omission',
+      'permittedParents' => 'অনুমোদিত প্যারেন্ট এলিমেন্ট সমূহ|অনুমোদিত parent এলিমেন্ট সমূহ',
+      'domInterface' => 'DOM ইন্টারফেস'
+    ],
     'ca' => [
       'contentCategories' => 'Content Categories de contingut',
       'permittedContent' => 'Contingut permès',
@@ -68,10 +77,14 @@
       'tagOmission' => 'Tag omission',
       'permittedParents' => 'Permitted parent elements',
       'domInterface' => 'DOM interface'
-    ]/*,
+    ],
     'de' => [
-      'permittedContent' => 'Erlaubter Inhalt'
-    ]*/,
+      'contentCategories' => 'Inhaltskategorien|Inhalts? Kategorien',
+      'permittedContent' => '(?:Zugelassener|Erlaubter) Inhalt',
+      'tagOmission' => 'Tag omission|Tagauslassung|Tag Auslassung|Tag Wegfall',
+      'permittedParents' => 'Zugelassene parent-Elemente|Erlaubte Eltern-Elemente|Erlaubte Elternelemente',
+      'domInterface' => 'DOM Schnittstelle'
+    ],
     'es' => [
       'contentCategories' => 'Content categories',
       'permittedContent' => 'Contenido permitido',
@@ -92,15 +105,35 @@
       'tagOmission' => 'Omission de balises',
       'permittedParents' => 'Éléments parents autorisés',
       'domInterface' => 'Interface DOM'
-    ]/*,
+    ],
     'he' => [
+      'contentCategories' => 'Content categories',
+      'permittedContent' => 'Permitted content',
+      'tagOmission' => 'Tag omission',
+      'permittedParents' => 'Permitted parent elements',
+      'domInterface' => 'DOM interface'
     ],
     'hu' => [
+      'contentCategories' => 'Tartalom kategóriák',
+      'permittedContent' => 'Engedélyezett tartalom',
+      'tagOmission' => 'Címke elhagyás',
+      'permittedParents' => 'Engedélyezett szülő elemek',
+      'domInterface' => 'DOM felület'
     ],
     'id' => [
+      'contentCategories' => 'Content categories',
+      'permittedContent' => 'Permitted content',
+      'tagOmission' => 'Tag omission',
+      'permittedParents' => 'Permitted parent elements',
+      'domInterface' => 'DOM interface'
     ],
     'it' => [
-    ]*/,
+      'contentCategories' => 'Aree tematiche',
+      'permittedContent' => 'Contenuto consentito',
+      'tagOmission' => 'Omissione Tag',
+      'permittedParents' => 'Elementi padre consentiti',
+      'domInterface' => 'Interfaccia DOM'
+    ],
     'ja' => [
       'contentCategories' => 'コンテンツカテゴリ',
       'permittedContent' => '許可された内容|利用可能な中身',
@@ -114,20 +147,34 @@
       'tagOmission' => '태그 생략',
       'permittedParents' => '허용된 부모 요소들',
       'domInterface' => 'DOM 인터페이스'
-    ]/*,
-    'ms' => [
     ],
+    'ms' => [
+      'contentCategories' => 'Content categories',
+      'permittedContent' => 'Permitted content',
+      'tagOmission' => 'Tag omission',
+      'permittedParents' => 'Permitted parent elements',
+      'domInterface' => 'DOM interface'
+    ]/*,
     'nl' => [
     ],
     'pl' => [
-    ],
-    'pt-BR' => [
-      'permittedContent' => 'Conteúdo permitido|Contepudo permitido'
-    ],
-    'pt-PT' => [
-    ],
-    'ro' => [
     ]*/,
+    'pt-BR' => [
+      'contentCategories' => 'Categorias de conteúdo',
+      'permittedContent' => 'Conteúdo permitido',
+      'tagOmission' => 'Omissão de tag',
+      'permittedParents' => 'Elementos pais permitidos',
+      'domInterface' => 'Interface DOM'
+    ]/*,
+    'pt-PT' => [
+    ]*/,
+    'ro' => [
+      'contentCategories' => 'Content categories',
+      'permittedContent' => 'Permitted content',
+      'tagOmission' => 'Tag omission',
+      'permittedParents' => 'Permitted parent elements',
+      'domInterface' => 'DOM interface'
+    ],
     'ru' => [
       'contentCategories' => 'Категории контента',
       'permittedContent' => 'Допустимое содержимое',
@@ -136,6 +183,11 @@
       'domInterface' => 'Интерфейс DOM'
     ],
     'tr' => [
+      'contentCategories' => 'Content categories',
+      'permittedContent' => 'Permitted content',
+      'tagOmission' => 'Tag omission',
+      'permittedParents' => 'Permitted parent elements',
+      'domInterface' => 'DOM interface'
     ],
     'uk' => [
     ],
@@ -172,6 +224,9 @@
     }
 
     preg_match_all('/<table class="standard-table">.+?<\/table>/s', $response, $categoryMatches);
+    if (count($categoryMatches[0]) === 0) {
+      preg_match_all('/<div class="index widgeted">.+?<\/div>/s', $response, $categoryMatches);
+    }
 
     $elementURLPaths = [];
     foreach ($categoryMatches[0] as $index => $categoryMatch) {
